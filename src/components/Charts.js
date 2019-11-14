31ae0aa296426661c4bff61f20eb663f45cac487
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2019-11-14 15:20:16 +0100

    add Components for sites

---
+++ src/components/Charts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Charts() {
+  return null;
+}
+
+export default Charts;
---
+++ src/components/Favourite.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Favourite() {
+  return null;
+}
+
+export default Favourite;
