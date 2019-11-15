e7941b92344f4145344de43ded133037a101e42a
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2019-11-15 16:43:00 +0100

    Added semantic-ui-react styles to form

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,24 +1,27 @@
 import React from "react";
 import * as Yup from "yup";
-import {Formik} from "formik";
+import { Formik } from "formik";
+import { Input, TextArea, Form } from "semantic-ui-react";
+import styles from "./AddForm.module.css";
 
 function AddForm() {
-  return(
-<div>
-     <label>NAZWA</label>
-    <input type="text" placeholder="NAME" id="name" />
-    <label>KRÓTKI OPIS</label>
-    <input type="text" placeholder="NAME" id="name" />
-    <label>ZDJĘCIE</label>
-    <input type="text" placeholder="NAME" id="name" />
-    <label>ADRES</label>
-    <input type="text" placeholder="NAME" id="name" />
-    <label>NR KONTAKTOWY</label>
-    <input type="text" placeholder="NAME" id="name" />
-    <label>DŁUGI OPIS</label>
-    <textarea type="text" placeholder="NAME" id="name" /> 
-   
-   </div>)
+  return (
+    <div className={styles.addForm}>
+
+      <label>NAMEs</label>
+      <Input focus type="text" placeholder="NAME" id="name" />
+      <label>SHORT DESCRIPTION</label>
+      <Input focus type="text" placeholder="SHORT DESCRIPTION" id="name" />
+      <label>IMAGE</label>
+      <Input focus type="text" placeholder="IMAGE" id="name" />
+      <label>ADDRESS</label>
+      <Input focus type="text" placeholder="ADDRESS" id="name" />
+      <label>PHONE NUMBER</label>
+      <Input focus type="text" placeholder="PHONE NUMBER" id="name" />
+      <label>LONG DESCRIPTION</label>
+      <Form><TextArea f rows={2} placeholder='Tell us more' /></Form>
+
+    </div>)
 }
 
 export default AddForm;
