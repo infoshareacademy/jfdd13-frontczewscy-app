3bf395eea2071e990d18fdc9dd43e693f82a4630
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2019-11-19 16:57:02 +0100

    rewrite Search and Sidebar Components

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,10 @@
-import React, { Fragment } from "react";
-import { Card, Icon } from "semantic-ui-react";
-import Sidebar from "./Sidebar";
-import styles from "./Search.module.css";
-import { posts } from "./data";
+import React, { Component } from "react";
+import { Button, Icon, Segment, Sidebar, Card } from "semantic-ui-react";
 
-const extra = (
-  <div>
-    <Icon name="user" />
-    16 Friends
-  </div>
-);
+import styles from "./Sidebar.module.css";
+import "react-input-range/lib/css/index.css";
+import { posts } from "./data";
+import VerticalSidebar from "./VerticalSidebar";
 
 const Item = props => {
   const { description, img, title } = props;
@@ -18,33 +13,80 @@ const Item = props => {
     <Card
       image={img}
       header={title}
-      meta="Friend"
       description={`${description.replace(/^(.{35}[^\s]*).*/, "$1")}...`}
-      extra={extra}
     />
   );
 };
 
-function Search() {
-  return (
-    <Fragment>
-      <Sidebar>
-        <div className={styles.content}>
-          <div className={styles.row}>
-            {posts.map(post => (
-              <div key={post.id} className={styles.item}>
-                <Item
-                  description={post.description}
-                  img={post.img}
-                  title={post.title}
-                />
+class SidebarSearch extends Component {
+  state = {
+    animation: "scale down",
+    direction: "right",
+    dimmed: false,
+    visible: true,
+    iconName: true,
+    filter: {
+      title: "",
+      sliderValue: null
+    }
+  };
+
+  handleAnimationChange = animation => () =>
+    this.setState(prevState => ({ animation, visible: !prevState.visible }));
+
+  handleDirectionChange = direction => () =>
+    this.setState({ direction, visible: false });
+
+  handleOnSearch = values => {
+    this.setState({
+      filter: values
+    });
+  };
+
+  render() {
+    const { animation, dimmed, direction, visible, filter } = this.state;
+    const vertical = direction === "bottom" || direction === "top";
+
+    return (
+      <div>
+        <Sidebar.Pushable as={Segment} className={styles.sidebar}>
+          <VerticalSidebar
+            onSearch={this.handleOnSearch}
+            animation={animation}
+            direction={direction}
+            visible={visible}
+            closeSidebar={this.handleAnimationChange}
+          />
+          <Button
+            className={styles.btn}
+            disabled={vertical}
+            onClick={this.handleAnimationChange("scale down")}>
+            <Icon name="bars" size="large" />
+          </Button>
+
+          <Sidebar.Pusher dimmed={dimmed && visible}>
+            <Segment basic>
+              <div className={styles.content}>
+                <div className={styles.row}>
+                  {posts
+                    .filter(post => post.title.includes(filter.title))
+                    .map(post => (
+                      <div key={post.id} className={styles.item}>
+                        <Item
+                          description={post.description}
+                          img={post.img}
+                          title={post.title}
+                        />
+                      </div>
+                    ))}
+                </div>
               </div>
-            ))}
-          </div>
-        </div>
-      </Sidebar>
-    </Fragment>
-  );
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div>
+    );
+  }
 }
 
-export default Search;
+export default SidebarSearch;
---
+++ src/components/VerticalSidebar.js
@@ -0,0 +1,105 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { Button, Icon, Menu, Sidebar, Input } from "semantic-ui-react";
+import { Formik } from "formik";
+import styles from "./Sidebar.module.css";
+import "react-input-range/lib/css/index.css";
+
+const TextInput = props => {
+  return (
+    <div className={styles.input}>
+      <Input {...props} />
+    </div>
+  );
+};
+
+const SelectInput = props => {
+  return (
+    <div>
+      <select className="ui selection dropdown dropdown" {...props}>
+        <option value="IMPREZA TANECZNA">IMPREZA TANECZNA</option>
+        <option value="KONCERT">KONCERT</option>
+        <option value="IMPREZA NIETANECZNA">IMPREZA NIETANECZNA</option>
+      </select>
+    </div>
+  );
+};
+
+const VerticalSidebar = props => {
+  const { animation, closeSidebar, direction, visible, onSearch } = props;
+  return (
+    <Sidebar
+      as={Menu}
+      animation={animation}
+      direction={direction}
+      icon="labeled"
+      inverted
+      vertical
+      visible={visible}
+      width="thin">
+      <Button className={styles.close} onClick={closeSidebar("scale down")}>
+        <Icon name="x" size="large" />
+      </Button>
+
+      <Formik
+        initialValues={{
+          title: "",
+          description: "",
+          partyType: "",
+          sliderValue: 10
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setSubmitting(true);
+          onSearch(values);
+        }}>
+        {({ values, handleChange, handleBlur, handleSubmit }) => {
+          return (
+            <form onSubmit={handleSubmit}>
+              <label className={styles.label}>Wyszukaj po nazwie</label>
+              <TextInput
+                type="text"
+                name="title"
+                placeholder="nazwa imprezy"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.title}
+              />
+              <label className={styles.label}>Kategoria</label>
+              <SelectInput
+                name="partyType"
+                placeholder="Rodzaj imprezy"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.partyType}
+              />
+              <div className={styles.slider}>
+                <div className={styles.sliderLabel}>Maksymalna cena [zł]</div>
+                <input
+                  type="range"
+                  name="sliderValue"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  min="0"
+                  max="100"
+                  value={values.sliderValue}
+                  step="5"
+                />
+                <div className={styles.value}>{values.sliderValue}</div>
+              </div>
+
+              <button type="submit">Submit</button>
+            </form>
+          );
+        }}
+      </Formik>
+    </Sidebar>
+  );
+};
+
+VerticalSidebar.propTypes = {
+  animation: PropTypes.string,
+  direction: PropTypes.string,
+  visible: PropTypes.bool
+};
+
+export default VerticalSidebar;
