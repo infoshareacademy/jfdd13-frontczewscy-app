86f1213177bb6c96ae4564b0f0365c1c8468aa21
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2019-11-20 16:45:16 +0100

    Added styles to PartDetails.js

--- src/components/PartyDetails.js
+++ src/components/PartyDetails.js
@@ -1,10 +1,12 @@
 import React from "react";
+import {List, Grid, Image, Header, Container} from "semantic-ui-react";
+import styles from "./PartyDetails.module.css";
 
 class Party extends React.Component {
   render() {
     const {
       title,
-      adress,
+      address,
       description,
       image,
       partyType,
@@ -12,22 +14,98 @@ class Party extends React.Component {
       price,
       website
     } = this.props.parties;
-
+    const DescriptionContainer = () => (
+      <Container style={{marginTop: '20px', width: '60%'}}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+          ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
+          magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+          ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+          quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+          arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+          Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
+          dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+          Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
+          Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+          viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
+          Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+          ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
+          magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+          ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+          quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+          arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+          Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
+          dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+          Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
+          Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+          viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
+          Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
+        </p>
+      </Container>)
+    const GridContainer = () => (
+      <Grid style={{width: '60%'}}>
+        <Grid.Column width={4}>
+          <Image src='https://react.semantic-ui.com/images/wireframe/image.png'  />
+        </Grid.Column>
+        <Grid.Column width={9}>
+        <List>
+            <Header style={{textAlign:"center"}} size='huge'>{title}</Header>
+            <List.Item icon='users' content={title} />
+            <List.Item icon='file text' content={description} />
+            <List.Item icon='money' content={price} />
+            <List.Item icon='marker' content={address} />
+            <List.Item
+              icon='mail'
+              content="mail@brakujetugo.pl"
+            />
+            <List.Item
+              icon='linkify'
+              content={<a href={website}>{website}</a>}
+            />
+          </List>
+        </Grid.Column>
+    
+      </Grid>
+    )
+    
+    const MainList = () => (
+      <List>
+        <List.Item icon='users' content={title} />
+        <List.Item icon='file text' content={description} />
+        <List.Item icon='money' content={price} />
+        <List.Item icon='marker' content={address} />
+        <List.Item
+          icon='mail'
+          content="mail@brakujetugo.pl"
+        />
+        <List.Item
+          icon='linkify'
+          content={<a href={website}>{website}</a>}
+        />
+      </List>)
     return (
-      <div>
-        <h1>Post title: {title}</h1>
-        <h2>Post adress: {adress}</h2>
-        <h2>Post description: {description}</h2>
-        <h2>Post image: {image}</h2>
-        <h2>Post partyType: {partyType}</h2>
-        <h2>Post phoneNumber: {phoneNumber}</h2>
-        <h2>Post price: {price}</h2>
-        <h2>Post website: {website}</h2>
-        <button onClick={console.log(this.props)}>X</button>
+      <div className={styles.mainGrid}>
+        <GridContainer className={styles.gridContainer}/>
+        <DescriptionContainer className={styles.descriptionContainer}/>
       </div>
+      // <div>
+      //   <h1>Post title: {title}</h1>
+      //   <h2>Post adress: {adress}</h2>
+      //   <h2>Post description: {description}</h2>
+      //   <h2>Post image: {image}</h2>
+      //   <h2>Post partyType: {partyType}</h2>
+      //   <h2>Post phoneNumber: {phoneNumber}</h2>
+      //   <h2>Post price: {price}</h2>
+      //   <h2>Post website: {website}</h2>
+      //   <button onClick={console.log(this.props)}>X</button>
+      // </div>
     );
   }
 }
+
 
 class PartyDetails extends React.Component {
   constructor(props) {
