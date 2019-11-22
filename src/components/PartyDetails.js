4082077378b53292cd6a5523ba8c8aa58398d3ec
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2019-11-22 15:11:12 +0100

    add email to the party details component

--- src/components/PartyDetails.js
+++ src/components/PartyDetails.js
@@ -1,5 +1,12 @@
 import React from "react";
-import {List, Grid, Image, Header, Container, Rating} from "semantic-ui-react";
+import {
+  List,
+  Grid,
+  Image,
+  Header,
+  Container,
+  Rating
+} from "semantic-ui-react";
 import styles from "./PartyDetails.module.css";
 
 class Party extends React.Component {
@@ -12,76 +19,78 @@ class Party extends React.Component {
       partyType,
       phoneNumber,
       price,
-      website
+      website,
+      email
     } = this.props.parties;
     const DescriptionContainer = () => (
-      <Container textAlign='justified'>
+      <Container textAlign="justified">
         <p>{description}</p>
-      </Container>)
+      </Container>
+    );
     const GridContainer = () => (
       <Grid centered>
         <Grid.Column width={6}>
-          <Image src={
-          image ||
-          "https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-        } style={{margin: '20px 0px'}} />
-         
+          <Image
+            src={
+              image ||
+              "https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            }
+            style={{ margin: "20px 0px" }}
+          />
         </Grid.Column>
         <Grid.Column width={10}>
-        <List>
-            <Header  size='huge'>{title}</Header>
-            
-            <List.Item>
-             <List.Icon name='users'   size="large" color='violet'/>
-             <List.Content>{title}</List.Content>
-            </List.Item>
-
+          <List>
+            <Header size="huge">{title}</Header>
 
             <List.Item>
-              <List.Icon name='phone square'   size="large" color='violet'/>
-              <List.Content>{phoneNumber || 'XXX-XXX-XXX'}</List.Content>
+              <List.Icon name="users" size="large" color="violet" />
+              <List.Content>{title}</List.Content>
             </List.Item>
 
             <List.Item>
-              <List.Icon name='check'   size="large" color='violet'/>
-              <List.Content>{partyType || "nie wybrano typu imprezy"}</List.Content>
+              <List.Icon name="phone square" size="large" color="violet" />
+              <List.Content>{phoneNumber || "XXX-XXX-XXX"}</List.Content>
             </List.Item>
 
             <List.Item>
-              <List.Icon name='money'  size="large" color='violet'/>
+              <List.Icon name="check" size="large" color="violet" />
+              <List.Content>
+                {partyType || "nie wybrano typu imprezy"}
+              </List.Content>
+            </List.Item>
+
+            <List.Item>
+              <List.Icon name="money" size="large" color="violet" />
               <List.Content>{price || "nie podano ceny"}</List.Content>
             </List.Item>
 
             <List.Item>
-              <List.Icon name='marker'  size="large" color='violet'/>
+              <List.Icon name="marker" size="large" color="violet" />
               <List.Content>{address || "brak adresu"}</List.Content>
             </List.Item>
 
-            <List.Item >
-            <List.Icon name='mail'  size="large" color='violet'/>
-              <List.Content>{ "nie podano adresu email"}</List.Content>
-
+            <List.Item>
+              <List.Icon name="mail" size="large" color="violet" />
+              <List.Content>{email || "brak adresu email"}</List.Content>
             </List.Item>
-            <List.Item >
-            <List.Icon name='linkify'  size="large" color='violet'/>
-              <List.Content>{ 0 || "nie podano adresu strony" }</List.Content>
+            <List.Item>
+              <List.Icon name="linkify" size="large" color="violet" />
+              <List.Content>{0 || "nie podano adresu strony"}</List.Content>
             </List.Item>
           </List>
-          <Rating icon='heart' defaultRating={8} maxRating={10} />
+          <Rating icon="heart" defaultRating={8} maxRating={10} />
         </Grid.Column>
-    
       </Grid>
-    )
-  
+    );
+
     return (
       <div className={styles.mainGrid}>
-        <GridContainer className={styles.gridContainer}/>
-        <DescriptionContainer className={styles.descriptionContainer}/>
+        <GridContainer className={styles.gridContainer} />
+        <DescriptionContainer className={styles.descriptionContainer} />
       </div>
     );
   }
 }
-
 
 class PartyDetails extends React.Component {
   constructor(props) {
@@ -115,17 +124,3 @@ class PartyDetails extends React.Component {
   }
 }
 export default PartyDetails;
-
-
-
-      // <div>
-      //   <h1>Post title: {title}</h1>
-      //   <h2>Post adress: {adress}</h2>
-      //   <h2>Post description: {description}</h2>
-      //   <h2>Post image: {image}</h2>
-      //   <h2>Post partyType: {partyType}</h2>
-      //   <h2>Post phoneNumber: {phoneNumber}</h2>
-      //   <h2>Post price: {price}</h2>
-      //   <h2>Post website: {website}</h2>
-      //   <button onClick={console.log(this.props)}>X</button>
-      // </div>
