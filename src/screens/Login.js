b1832749dbbb44fb01bfd61bc0e8a1d2f866d153
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2019-11-26 16:38:31 +0100

    Added Login i Hasło input

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,15 +3,12 @@ import { Button, Checkbox, Form } from "semantic-ui-react";
 const Login = () => {
   return   <Form>
   <Form.Field>
-    <label>First Name</label>
+    <label>Login</label>
     <input placeholder='First Name' />
   </Form.Field>
   <Form.Field>
-    <label>Last Name</label>
-    <input placeholder='Last Name' />
-  </Form.Field>
-  <Form.Field>
-    <Checkbox label='I agree to the Terms and Conditions' />
+    <label>Hasło</label>
+    <input type="password" placeholder='Last Name' />
   </Form.Field>
   <Button type='submit'>Submit</Button>
 </Form>
