fa6bc665944ec698ba09864857d1a4a1e68153ab
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2019-11-27 14:29:12 +0100

    Added gender select adn profile image inputs

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -5,28 +5,40 @@ import styles from "./Register.module.css"
 const Register = () => {
   return  <Form className={styles.registerForm}>
     <Message size="small" className={styles.registerFormMessage}
-    icon='registered outline'
+    icon='add user'
     header='Witaj w formularzu rejestracji.'
     content='Wypełnij wszystkie pola, kliknij zatwierdź aby zakończyć rejestrację.'
   />
   
-  <Form.Field className={styles.registerFormEmail}>
+  <Form.Field required className={styles.registerFormEmail}>
     <label>E-mail</label>
     <input placeholder='E-mail' />
   </Form.Field>
-  <Form.Field className={styles.registerFormName}>
+  <Form.Field required className={styles.registerFormName}>
     <label>Name</label>
     <input placeholder='Name' />
   </Form.Field>
-  <Form.Field className={styles.registerFormPass}>
+  <Form.Field required className={styles.registerFormPass}>
     <label>Hasło</label>
     <input type="password" placeholder='Hasło' />
   </Form.Field>
-  <Form.Field className={styles.registerFormPassRep}>
+  <Form.Field required className={styles.registerFormPassRep}>
     <label>Powtórz hasło</label>
     <input type="password" placeholder='Powtórz hasło' />
   </Form.Field>
-  <Form.Field className={styles.registerFormBio}>
+  <Form.Field className={styles.registerFormImage}>
+    <label>Zdjęcie profilowe</label>
+    <input placeholder='Zdjęcie profilowe' />
+  </Form.Field>
+  <Form.Field className={styles.registerFormSelect}>
+    <label>Płeć</label>
+    <select>
+      <option>Mężczyzna</option>
+      <option>Kobieta</option>
+      <option>Nie chcę podawać</option>
+    </select>
+  </Form.Field>
+  <Form.Field  className={styles.registerFormBio}>
     <label>Bio</label>
     <textarea placeholder="Bio"  style={{minHeight:"200px"}} rows="3"></textarea>
   </Form.Field>
