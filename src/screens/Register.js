a7049dca2dcfa1099b5f40967a9ac275a5840a86
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2019-11-27 12:35:01 +0100

    Added registration form

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Button, Checkbox, Form } from "semantic-ui-react";
+import styles from "./Register.module.css"
 
 const Register = () => {
-  return <h1>Register</h1>;
+  return  <Form >
+  <Form.Field >
+    <label>E-mail</label>
+    <input placeholder='E-mail' />
+  </Form.Field>
+  <Form.Field >
+    <label>Name</label>
+    <input placeholder='Name' />
+  </Form.Field>
+  <Form.Field >
+    <label>Hasło</label>
+    <input type="password" placeholder='Hasło' />
+  </Form.Field>
+  <Form.Field>
+    <label>Powtórz hasło</label>
+    <input type="password" placeholder='Powtórz hasło' />
+  </Form.Field>
+  <Form.Field>
+    <label>Bio</label>
+    <input type="text" placeholder='Bio' />
+  </Form.Field>
+  <Button type='submit'>Submit</Button>
+</Form>
 };
 
 export default Register;
