e6b0e5b4a2c8e50d1567970037027c3c3346a1c1
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2019-11-28 16:22:54 +0100

    Some crucial changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyClsb3yVGx9MrVEfZjNtz_637QN166iqWk",
+  apiKey: "XD",
   authDomain: "frontczewscy-database.firebaseapp.com",
   databaseURL: "https://frontczewscy-database.firebaseio.com",
   projectId: "frontczewscy-database",
   storageBucket: "frontczewscy-database.appspot.com",
-  messagingSenderId: "965209740516",
-  appId: "1:965209740516:web:7464923ca514d3358388f1",
-  measurementId: "G-9SRXWYBECN"
+  messagingSenderId: "XD",
+  appId: "XD",
+  measurementId: "XD"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
