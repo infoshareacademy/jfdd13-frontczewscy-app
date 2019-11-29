89c39e32389cf446a0848e725196eb1ec98851f7
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2019-11-29 16:38:32 +0100

    Added logic and validattion to checkbox input in register

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -38,6 +38,8 @@ const accountFormSchema = Yup.object().shape({
     .min(3, "Min 3 znaki!")
     .max(15, "Max 15 znaków")
     .required("Pole wymagane"),
+  registerCheckbox: 
+    Yup.boolean().oneOf([true], 'Musisz zaakceptować warunki korzystania z naszej strony.'),
   email: Yup.string()
     .max(100, "Za długi email")
     .email("Zły format email")
@@ -55,6 +57,27 @@ const TextInput = props => {
     
         </div>
         <Input style={{width:"80%"}} {...props} error={errors[name] && touched[name]} />{" "}
+      </label>
+      <div className={styles.error}>
+        {errors[name] && touched[name] && errors[name]}
+      </div>
+    </div>
+  );
+};
+
+const CheckboxInput = props => {
+  const { name, errors, touched, labelform, tooltiptext, labelRequire } = props;
+  return (
+    <div>
+      <label >
+        <div style={{width:"80%"}} className={styles.tooltip}>
+        
+          <span className={styles.star}>{labelRequire}</span>
+    
+        </div>
+        <div className={styles.inputDiv}>
+        <Checkbox  style={{width:"20px", marginRight:"10px"}} {...props} error={errors[name] && touched[name]} />{" "}
+        <label>{labelform}</label></div>
       </label>
       <div className={styles.error}>
         {errors[name] && touched[name] && errors[name]}
@@ -125,11 +148,13 @@ class Register extends React.Component {
             passwordRep: "",
             image: "",
             bio: "",
-            joined: moment().format('L')
+            joined: moment().format('L'),
+            registerCheckbox: "false"
           }}
           validationSchema={accountFormSchema}
           onSubmit={(values, { setSubmitting, resetForm }) => {
             setSubmitting(true);
+            console.log(values)
             
             this.setState({ btnLoading: true, btnDisabled: true });
 
@@ -235,12 +260,23 @@ class Register extends React.Component {
                     touched={touched}
                     errors={errors}
                   />
-                    
-                    <div className={styles.checkboxInput}>
+                        <CheckboxInput
+                        id="checkbox"
+                    type="checkbox"
+                    name="registerCheckbox"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.registerCheckbox}
+                    touched={touched}
+                    errors={errors}
+                    labelform="Akceptuję warunki korzystania ze strony."
+                  />
+               
+                    {/* <div className={styles.checkboxInput}>
               <div class="ui checkbox" style={{display:"flex", justifyContent:"center"}}>
                 <input type="checkbox" id="regCheckbox" />
                 <label htmlFor="regCheckbox" style={{width:"250px", cursor:"pointer"}}>Zgoda na przetwarzanie danych</label>
-              </div></div>
+              </div></div> */}
                   <Button
                     style={{ marginTop: "10px" }}
                     className={styles.formBtn}
