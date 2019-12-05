e6f092c7d594d3e1e6507099f63076f3d7e603e6
Maria <mariaromancz@gmail.com>
2019-12-05 14:51:34 +0100

    hide menu button added and works

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
 import { NavLink } from "react-router-dom";
 import { Icon } from 'semantic-ui-react';
@@ -12,29 +12,30 @@ import firebase from "../firebase";
  const Profile = () => <Icon disabled name='user' />
  const SignOut = () => <Icon disabled name='sign-out' />
 
- const Navbar = () => (
-  <nav className={styles.navbar}>
-      <div className={styles.left}>
-        <NavLink to="/" exact className={styles.navLogo}>
-          <img className={styles.logo} src = { Logo } alt="Concerte Logo" />
-          <span>Concerte</span>
-        </NavLink>
-        <button className={styles.navButton} onClick={() => {moveNavbar()}}>▼</button>
-      </div>
-      <ul className={styles.right}>
-          <li><NavLink activeClassName={"active-link"} to="/wyszukaj"exact><Search />Wyszukiwanie imprez</NavLink></li>
-          <li><NavLink activeClassName={"active-link"} to="/"exact><Chart />Wykresy</NavLink></li>
-          <li><NavLink activeClassName={"active-link"} to="/dodaj-wydarzenie"exact><AddEvent />Dodaj wydarzenie</NavLink></li>
-          <li><NavLink activeClassName={"active-link"} to="/profil"exact><Profile />Profil</NavLink></li>
-          <li onClick={() => firebase.auth().signOut()}><a href="#"><SignOut />Wyloguj</a></li>
-      </ul>
-      
-    </nav>
-  );
+ const Navbar = () => { 
 
-function moveNavbar() {
-  var butt = document.getElementsByClassName("navButton");
-  butt.classList.add("disappear");
-}
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+
+  return (<nav className={styles.navbar}>
+    <div className={styles.left}>
+      <NavLink to="/" exact className={styles.navLogo}>
+        <img className={styles.logo} src = { Logo } alt="Concerte Logo" />
+        <span>Concerte</span>
+      </NavLink>
+      <button className={styles.navButton} onClick={() => {setIsNavOpen(!isNavOpen)}}>▼</button>
+    </div>
+    <ul className={styles.right}   style={ { display: isNavOpen ? 'block' : 'none' } } >
+        <li><NavLink activeClassName={"active-link"} to="/wyszukaj"exact><Search />Wyszukiwanie imprez</NavLink></li>
+        <li><NavLink activeClassName={"active-link"} to="/"exact><Chart />Wykresy</NavLink></li>
+        <li><NavLink activeClassName={"active-link"} to="/dodaj-wydarzenie"exact><AddEvent />Dodaj wydarzenie</NavLink></li>
+        <li><NavLink activeClassName={"active-link"} to="/profil"exact><Profile />Profil</NavLink></li>
+        <li onClick={() => firebase.auth().signOut()}><a href="#"><SignOut />Wyloguj</a></li>
+    </ul>
+    
+  </nav>)
+  
+ };
+
 
 export default Navbar;
